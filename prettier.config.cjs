7d7d8a934b8decee9c926c378ac01047b46926c4
kiko-g <kikojpgoncalves@gmail.com>
2023-03-04 20:23:07 +0000

    update: format files and add envs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  printWidth: 100,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
 };
 
 module.exports = config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0d9488',
+        secondary: '#3b82f6',
+        navy: '#1a202c',
+        dark: '#242936',
+        darker: '#1e222c',
+        darkest: '#1a1e28',
+        darkish: '#333640',
+        light: '#f2f4f7',
+        lighter: '#f7f7f7',
+        lightest: '#fcfcfc',
+        lightish: '#ebedf0',
+      },
+      maxWidth: {
+        screen: '100vw',
+      },
+      fontSize: {
+        xxs: '0.65rem',
+      },
+      fontFamily: {
+        inter: ['Inter', ...defaultTheme.fontFamily.sans],
+        lexend: ['Lexend', ...defaultTheme.fontFamily.sans],
+        code: ['Fira Code', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
