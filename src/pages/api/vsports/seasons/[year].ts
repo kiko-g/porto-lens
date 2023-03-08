6cc73476e02cc463c5bd50e17adb024f18e00c3e
kiko-g <kikojpgoncalvesgmail.com>
2023-03-08 21:24:11 +0000

    update: rename envs in api

--- src/pages/api/vsports/seasons/[year].ts
+++ src/pages/api/vsports/seasons/[year].ts
@@ -3,9 +3,9 @@ import { type Season } from '~/@types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const s3 = new S3({
-  region: process.env.AWS_S3_REGION_NAME,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.NEXT_PUBLIC_AWS_S3_REGION_NAME,
+  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
 });
 
 const getDataFromS3 = async (bucketName: string, object: S3.Object): Promise<Season | null> => {
@@ -25,7 +25,7 @@ const getDataFromS3 = async (bucketName: string, object: S3.Object): Promise<Sea
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const bucketName = process.env.AWS_S3_BUCKET_NAME || 'vsports';
+    const bucketName = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME || 'vsports';
     const data = await s3.listObjectsV2({ Bucket: bucketName }).promise();
 
     if (!data.Contents) {
